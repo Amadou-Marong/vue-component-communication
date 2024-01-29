80fd24c54d472a02e4ecec25ef9b4951cb173125
Amadou-Marong <marongamadou6@gmail.com>
2024-01-29 10:18:01 +0000

    working with communicating-components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,52 @@
+<template>
+  <li>
+    <h2>{{ name }} {{friendIsFavourite === '1' ? '(Favourite)' : ''}}</h2>
+    <button @click="toggleFavorite">toggleFavorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'name', 
+    'phoneNumber', 
+    'emailAddress', 
+    'isFavorite'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+      friendIsFavourite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavourite === '1'){
+        this.friendIsFavourite = '0';
+      }else{
+        this.friendIsFavourite = '1';
+      }  
+    }
+  }
+};
+</script>
